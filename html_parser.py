e39c04debd8c191d89c4d228944aba0345bfe414
dagou <jbq19920903@163.com>
2017-11-06 09:18:14 +0800

    init

--- html_parser.py
+++ html_parser.py
@@ -14,6 +14,7 @@ class HtmlParser(object):
     def parse(self, page_url, html_cont):
         if page_url is None or html_cont is None:
             return
+        # 构建网页解析器
         soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
 
         new_urls = self._get_new_urls(page_url, soup)
@@ -26,8 +27,12 @@ class HtmlParser(object):
         links = soup.find_all('a', href=re.compile(r"/item/"))
         for link in links:
             new_url = link['href']
+
+            # 拼接完整的url
             new_full_url = urllib.parse.urljoin(page_url, new_url)
+
             new_urls.add(new_full_url)
+
         return new_urls
 
     def _get_new_data(self, page_url, soup):
@@ -42,4 +47,5 @@ class HtmlParser(object):
         summary_node = soup.find('div', class_='lemma-summary')
         res_data['summary'] = summary_node.get_text()
 
+        # 三个属性：url、title、summary
         return res_data
