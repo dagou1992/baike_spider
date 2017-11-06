78aeb360d645eaf0430a7ec4da5c8a1f937c9452
dagou <jbq19920903@163.com>
2017-11-06 15:25:07 +0800

    export excel

--- html_outputer.py
+++ html_outputer.py
@@ -3,6 +3,8 @@
 
 __author__ = 'King'
 
+import xlwt
+
 class HtmlOutputer(object):
     def __init__(self):
         self.datas = []
@@ -15,19 +17,44 @@ class HtmlOutputer(object):
 
 
     def output_html(self):
-        fout = open('output.html', 'w', encoding='utf-8')
-        fout.write('<html>')
-        fout.write('<body>')
-        fout.write('<table>')
 
+        # fout = open('output.html', 'w', encoding='utf-8')
+        # fout.write('<html>')
+        # fout.write('<body>')
+        # fout.write('<table>')
+        #
+        # for data in self.datas:
+        #     fout.write('<tr>')
+        #     fout.write("<td><a href=%s>" % data['url'])
+        #     fout.write('%s</a></td>' % data['title'])
+        #     fout.write('<td>%s</td>' % data['summary'])
+        #     fout.write('</tr>')
+        #
+        # fout.write('</table>')
+        # fout.write('</body>')
+        # fout.write('</html>')
+        # fout.close()
+
+        print('准备将数据存入表格...')
+
+        book = xlwt.Workbook()
+        sheet1 = book.add_sheet('sheet1', cell_overwrite_ok=True)
+
+        # 先将表头保存
+        heads = [u'No', u'title', u'summary', u'url']
+        ii = 0
+        for head in heads:
+            sheet1.write(0, ii, head)
+            ii += 1
+
+        # 导入数据
+        i = 1
         for data in self.datas:
-            fout.write('<tr>')
-            fout.write("<td><a href=%s>" % data['url'])
-            fout.write('%s</a></td>' % data['title'])
-            fout.write('<td>%s</td>' % data['summary'])
-            fout.write('</tr>')
+            sheet1.write(i, 0, i)
+            sheet1.write(i, 1, data['title'])
+            sheet1.write(i, 2, data['summary'])
+            sheet1.write(i, 3, data['url'])
+            i += 1
 
-        fout.write('</table>')
-        fout.write('</body>')
-        fout.write('</html>')
-        fout.close()
+        book.save('Python_Baike_Data.xls')
+        print('保存成功')
